ebffc67d89773a868a3aaa3a656ead729c8b7272
Sumit Soreng <sumitsoreng2000@gmail.com>
2026-08-16 22:08:08 +0530

    Log Aggregator project initialization

---
+++ src/main/java/com/log/aggregator/LogAggregator.java
@@ -0,0 +1,8 @@
+package com.log.aggregator;
+
+public class LogAggregator{
+
+    public static void main(String[] args){
+        System.out.println("Hello to production grade prototype of log aggregator tool");
+    }
+}
